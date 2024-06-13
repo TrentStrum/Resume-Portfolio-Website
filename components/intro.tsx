8de9003ede10c69aacaedbdc3aac9858432a0a79
tstrum <trent.strum@gmail.com>
2024-06-12 19:52:22 -0700

    resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,7 +92,7 @@ export default function Intro() {
 
 				<a
 					className='group bg-white px-7 py-3 flex items-center gap-1.5 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
-					href='public\Trent Resume.pdf'
+					href='./Trent Resume.pdf'
 					download
 				>
 					Download CV{' '}
